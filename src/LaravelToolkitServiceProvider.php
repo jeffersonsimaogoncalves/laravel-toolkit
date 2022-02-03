5800ecaf07943feea6c83c8e62feca6a3fb5eb5b
Steve McDougall <juststevemcd@gmail.com>
2022-02-03 10:14:54 +0000

    Adding a service provider

---
+++ src/LaravelToolkitServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustSteveKing\LaravelToolkit;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelToolkitServiceProvider extends ServiceProviderProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
